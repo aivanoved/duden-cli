c1e901b5a3d479c679081b089d71c1a669ea93ef
Alexander Ivanov <54221777+aivanoved@users.noreply.github.com>
2025-08-19 00:49:34 +0300

    feat: add hints

--- duden_cli/cli.py
+++ duden_cli/cli.py
@@ -1,7 +1,7 @@
 import structlog
 import typer
 
-from duden_cli.definition import definition
+from duden_cli.definition import SingleMeaning, definition
 
 log = structlog.get_logger()
 
@@ -19,24 +19,47 @@ def meaning(word: str) -> None:
     print(output)
 
 
+def hint_from_definition(definition: SingleMeaning) -> str:
+    defined = definition.meaning.split()
+    enumerated = " ".join(
+        list(
+            [f"{word} /{idx}/" for idx, word in enumerate(defined)],
+        )
+    )
+    idx = int(
+        input(f"Enter the number of the hint word or -1 for no hint: {enumerated}\n")
+    )
+
+    return f"/{defined[idx]}/" if idx != -1 else ""
+
+
 @cli.command()
 def gen_deck() -> None:
     import genanki
 
     import duden_cli.anki as anki
 
-    word = input("Ask for word (q for quit):")
+    word = input("Ask for word (q for quit): ")
     while word != "q":
-        output = definition(word)
+        try:
+            output = definition(word)
+        except:  # noqa: E722
+            log.error("Encountered an error: '%s'", word)
+            output = None
+
         if output is None:
             log.info("No card generated for the word: '%s'", word)
             word = input("Ask for word (q for quit): ")
             continue
 
         for def_ in output.definition.definitions:
+            hint = None
+            if len(output.definition.definitions) > 1:
+                hint = hint or hint_from_definition(def_)
+
             note = genanki.Note(
                 model=anki.model,
-                fields=[output.word, def_.meaning],
+                fields=[output.word, def_.meaning, hint or ""],
             )
 
             anki.deck.add_note(note)
